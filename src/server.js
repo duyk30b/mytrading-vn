6d82d994e4f01f4fce71c074087aa3cec51ec98f
duyk30b <49905710+duyk30b@users.noreply.github.com>
2021-04-14 22:59:52 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>{
+    res.send('Hello World, This is mytrading-vn homepage')
+})
+
+app.post('/', (req, res) =>{
+    console.log(req.body);
+    res.send({'testJson':'succes'})
+})
+
+app.listen(port, ()=>{
+    console.log('Domain listening at http://localhost:' + port)
+})
